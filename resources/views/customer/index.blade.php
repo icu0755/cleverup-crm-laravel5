6e591635e1e3d4d3aaf608f79c2023967db6d725
Vladimir Ivanov <vladimir.ivanov@fastdev.se>
2015-04-06 22:39:59 +0400

    Customer index refactoring

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,16 +1,18 @@
 @extends('layout')
 
-@section('sidebar')
-<div class="col-sm-3 col-md-2 sidebar">
-    <ul class="nav nav-sidebar">
-        <li>{!! link_to_route('customers.addForm', 'Add customer') !!}</li>
-    </ul>
-</div>
-@stop
-
 @section('content')
-<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-    <h1 class="page-header">Customers</h1>
-    <table class="customers-table" cellspacing="0" width="100%"></table>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-2">
+            <ul class="nav nav-sidebar">
+                <li>{!! link_to_route('customers.addForm', 'Add customer') !!}</li>
+            </ul>
+        </div>
+
+        <div class="col-md-9 main">
+            <h1 class="page-header">Customers</h1>
+            <table class="customers-table" cellspacing="0" width="100%"></table>
+        </div>
+    </div>
 </div>
 @stop
