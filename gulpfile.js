0ef3ba070833a702aba616fb8b1c5793d9ecf74a
Vladimir Ivanov <vladimir.ivanov@fastdev.se>
2015-05-13 21:10:26 +0400

    Add bower.json, set up gulp

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,28 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.less('app.less');
+    //mix.less('app.less');
+    mix.scripts([
+        '../bower/jquery/dist/jquery.js',
+        '../bower/moment/moment.js',
+        '../bower/fullcalendar/dist/fullcalendar.js',
+        '../bower/datatables/media/js/jquery.dataTables.js',
+        '../bower/jt.timepicker/jquery.timepicker.js',
+        '../bower/datepair.js/dist/datepair.js',
+        '../bower/datepair.js/dist/jquery.datepair.js',
+    ], 'public/scripts/vendor.js');
+    mix.scripts([
+       '../bower/bootstrap/js/affix.js',
+       '../bower/bootstrap/js/alert.js',
+       '../bower/bootstrap/js/dropdown.js',
+       '../bower/bootstrap/js/tooltip.js',
+       '../bower/bootstrap/js/modal.js',
+       '../bower/bootstrap/js/transition.js',
+       '../bower/bootstrap/js/button.js',
+       '../bower/bootstrap/js/popover.js',
+       '../bower/bootstrap/js/carousel.js',
+       '../bower/bootstrap/js/scrollspy.js',
+       '../bower/bootstrap/js/collapse.js',
+       '../bower/bootstrap/js/tab.js',
+    ], 'public/scripts/plugins.js');
 });
